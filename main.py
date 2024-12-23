4b57250f0c7e7e9a3c73d0277a334e58e0e84d63
batman565 <mixail_dremov@mail.ru>
2024-12-23 10:30:36 +0300

    add files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from numpy import sin
-from people import get_employees
-from salary import calculate_salary
+from people.people import get_employees
+from salary.salary import calculate_salary
 
 
 if __name__ == "__main__":
